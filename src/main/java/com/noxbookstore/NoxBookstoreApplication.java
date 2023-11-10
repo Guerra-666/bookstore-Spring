170ff18c555a712d13ea51bd76ba36c6924408da
Adrian Guerra <AdrianGuerra24666@gmail.com>
2023-11-10 16:33:47 -0600

    First commit, upload of files

---
+++ src/main/java/com/noxbookstore/NoxBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.noxbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoxBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoxBookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noxbookstore/NoxBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noxbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoxBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
