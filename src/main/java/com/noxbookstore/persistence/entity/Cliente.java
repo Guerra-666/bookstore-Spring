06acee60a2e5c078c2b51f726634077cf494b5e1
Adrian Guerra <AdrianGuerra24666@gmail.com>
2023-11-10 19:43:06 -0600

    Getters and Setters Generated

---
+++ src/main/java/com/noxbookstore/persistence/entity/Cliente.java
@@ -0,0 +1,69 @@
+package com.noxbookstore.persistence.entity;
+
+import jakarta.persistence.*;
+@Table(name = "Clientes")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_cliente")
+    private Integer idCliente;
+
+    private String nombre;
+
+    private String apellidos;
+
+    private Integer celular;
+
+    private String direccion;
+
+    private String correo;
+
+    public Integer getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Integer idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Integer getCelular() {
+        return celular;
+    }
+
+    public void setCelular(Integer celular) {
+        this.celular = celular;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+}
